39c3e2e73e8e8e7651bd7d24afb4e5f268e62194
mikaelec <mikaelec@gmail.com>
2016-02-10 18:39:36 +0100

    Added point calculation.

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-var point = 200;
+var points = 2000;
 
 $(document).ready(function () {
   $('#playButton').click(() => {
@@ -7,6 +7,8 @@ $(document).ready(function () {
 });
 
 function gameF() {
+  points -= 100;
+
   var syms = Array.apply(null, Array(3)).map(x => {
     return Math.ceil(Math.random(parseInt(new Date().toString(2)))*3);
   });
@@ -15,4 +17,22 @@ function gameF() {
     var img = `url(grafik/step${x}.png)`;
     $(`#symbol${i+1}`).css('background-image', img);
   });
+
+  var result = syms.reduce((prev, cur, i) => {
+    return prev + cur;
+  }, "");
+
+  switch (result) {
+    case "111":
+      points += 400;
+      break;
+    case "222":
+      points += 800;
+      break;
+    case "333":
+      points += 1200;
+      break;
+  }
+
+  $('#points').html(points);
 }
