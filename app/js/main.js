fa2c95b292f32600dc40554a1f58379af5deef77
mikaelec <mikaelec@gmail.com>
2016-02-10 18:32:14 +0100

    Added random effect of game.

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,18 @@
-// JavaScript Document
+var point = 200;
+
+$(document).ready(function () {
+  $('#playButton').click(() => {
+    gameF();
+  });
+});
+
+function gameF() {
+  var syms = Array.apply(null, Array(3)).map(x => {
+    return Math.ceil(Math.random(parseInt(new Date().toString(2)))*3);
+  });
+
+  syms.forEach((x,i) => {
+    var img = `url(grafik/step${x}.png)`;
+    $(`#symbol${i+1}`).css('background-image', img);
+  });
+}
